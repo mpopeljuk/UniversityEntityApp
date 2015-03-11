b7f8ccd142193d761f16494dbbc794d5455d1a65
mpopeljuk <mpopeljuk@gmail.com>
2015-03-11 11:29:09 +0200

    Trying to make singlepage

---
+++ UniversityAPI/Content/JavaScript/mainScript.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="D:\SS_Projects\UniversityEntityApp\UniversityAPI\Scripts/jquery-2.1.3.intellisense.js" />
+/// <reference path="D:\SS_Projects\UniversityEntityApp\UniversityAPI\Scripts/jquery-2.1.3.js" />
+
+
+$(function () {
+    var $studentDialog = $('#dialog-window');
+    var $studentForm = $studentDialog.find('form');
+
+    $studentDialog.dialog({
+        autoOpen: false,
+        modal: true,
+        resizable: false,
+        draggable: false
+    });
+
+    var $openDialogButton = $('#open-dialog');
+    $openDialogButton.click(function () {
+        $studentDialog.dialog('open');
+    });
+});
