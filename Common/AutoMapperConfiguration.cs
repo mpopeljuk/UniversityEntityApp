a0e9f81e48f484db32842428a684b037498bff2d
mpopeljuk <mpopeljuk@gmail.com>
2015-03-12 19:11:17 +0200

    Whaaaat?

---
+++ Common/AutoMapperConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(
+                cfg => {
+                    cfg.AddProfile(new MappingProfile());
+                }
+            );
+        }
+    }
+}
---
+++ Common/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using DBModels;
+using DBModels.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class MappingProfile : Profile
+    {
+        protected override void Configure()
+        {
+            base.Configure();
+
+            Mapper.CreateMap<GroupDTO, Group>();
+            Mapper.CreateMap<Group, GroupDTO>();
+
+            Mapper.CreateMap<StudentDTO, Student>();
+            Mapper.CreateMap<Student, StudentDTO>();
+
+            Mapper.CreateMap<SubjectDTO, Subject>();
+            Mapper.CreateMap<Subject, SubjectDTO>();
+        }
+    }
+}
